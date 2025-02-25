3361be8165259f5628281ca6f5e2bb19b821636c
Jan Härkönen <jan.harkonen@gmail.com>
2025-02-25 14:32:28 +0200

    make a dummy front end

--- Svelte-files/src/routes/+page.svelte
+++ Svelte-files/src/routes/+page.svelte
@@ -1,11 +1,13 @@
 <script>
-  let count = 0;
-
-  function increment() {
-    count += 1;
-  }
+  let panel1 = 0;
+  let panel2 = 0;
+  let panel3 = 0;
+  let panel4 = 0;
 </script>
 
-<button on:click={increment}>
-  Clicked {count} {count === 1 ? 'time' : 'times'}
-</button>
+<div>
+  <p>Row 1: {panel1}</p> 
+  <p>Row 2: {panel2}</p> 
+  <p>Row 3: {panel3}</p> 
+  <p>Row 4: {panel4}</p> 
+</div>
